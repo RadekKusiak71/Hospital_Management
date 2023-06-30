b753fe50a1fe5c18e5a7c7d42e4a6395515b10ad
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-07-01 01:13:23 +0200

    database register

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from main.models import Patient,Doctor,Apointment,Medicine,Perscription,Meeting,Room,Message
 
-# Register your models here.
+admin.site.register(Patient)
+admin.site.register(Doctor)
+admin.site.register(Apointment)
+admin.site.register(Medicine)
+admin.site.register(Perscription)
+admin.site.register(Meeting)
+admin.site.register(Room)
+admin.site.register(Message)
