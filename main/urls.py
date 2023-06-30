edac833f46775520081b1002e7fbec8a5cc001d4
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-07-01 01:13:43 +0200

    urls added

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.HomePage.as_view(),name='home_page'),
+    path('',views.DashboardPage.as_view(),name='dashboard'),
+    path('docreg/',views.DoctorRegisterPage.as_view(),name='doctor_register_page'),
+    path('doclog/',views.DoctorLoginPage.as_view(),name='doctor_login_page'),
+    path('doclogout/',views.DoctorLogout.as_view(),name='doctor_logout'),
+    path('calendar/',views.CalendarPage.as_view(),name='doctor_calendar'),
 ]
