8689cade8b3e6c36d182a6b6b885a7c5269bb9aa
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-07-01 23:27:17 +0200

    Appointments calendar backed done

--- main/views.py
+++ main/views.py
@@ -1,7 +1,11 @@
+import calendar
+import datetime
+from datetime import timedelta
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.views import View
 from django.utils import timezone
+from django.contrib import messages
 from django.contrib.auth import logout,login,authenticate
 from django.utils.decorators import method_decorator
 from django.contrib.admin.views.decorators import staff_member_required
@@ -66,12 +70,75 @@ class DashboardPage(View):
     
 method_decorator(staff_member_required,name='dispatch')
 class CalendarPage(View):
-    def get(self,request):
+    today = datetime.date.today()
+
+    def get(self, request):
         if request.user.is_staff:
-            context = {}
-            return render(request,'main/doctor_calendar.html',context)
+            day = datetime.date.today()
+            calendar = self.get_calendar(CalendarPage.today)
+            days = self.get_days(calendar)
+            appointments = self.get_user_appointments()
+            days_list = self.update_list(appointments,days)
+
+            if 'next' in request.GET:
+                CalendarPage.today += timedelta(days=31)
+                calendar = self.get_month(CalendarPage.today)
+                days = self.get_days(calendar)
+                days_list = self.update_list(appointments,days)
+                return redirect('doctor_calendar')
+            
+            elif 'last' in request.GET:
+                if CalendarPage.today < datetime.date.today():
+                    calendar = self.get_month(CalendarPage.today)
+                    return redirect('doctor_calendar')
+                else:
+                    CalendarPage.today -= timedelta(days=31)
+                    calendar = self.get_month(CalendarPage.today)
+                    days = self.get_days(calendar)
+                    days_list = self.update_list(appointments,days)
+                    return redirect('doctor_calendar')
+                
+            elif 'start_day' in request.GET:
+                    CalendarPage.today = datetime.date.today()
+                    calendar = self.get_month(CalendarPage.today)
+                    days = self.get_days(calendar)
+                    days_list = self.update_list(appointments,days)
+                    return redirect('doctor_calendar')
+            
+            context = {'calendar': calendar, 'days': days_list,'today_date':day,'appointments':appointments}
+            return render(request, 'main/doctor_calendar.html', context)
         else:
             return HttpResponse('Only for staff members')
+        
+    def update_list(self, appointments, day_list):
+        days_l = [[day] for day in day_list]
+        for appointment in appointments:
+            for day in days_l:
+                if day[0].strftime("%Y-%m-%d") == appointment.date.strftime("%Y-%m-%d"):
+                    day.append(appointment)
+        return days_l
+
+    def get_calendar(self,today):
+        cal = calendar.Calendar()
+        month_calendar = list(cal.monthdatescalendar(today.year,today.month))
+        return month_calendar
+    
+    def get_days(self,month_calendar):
+        days_list = []
+        for week in month_calendar:
+            for date in week:
+                days_list.append(date)
+        return days_list
+    
+    def get_user_appointments(self):
+        doctor = Doctor.objects.get(user=self.request.user)
+        appointments = Apointment.objects.filter(doctor=doctor).order_by('date')
+        return appointments
+    
+    def get_month(self,today):
+        cal = calendar.Calendar()
+        month_calendar = list(cal.monthdatescalendar(today.year,today.month))
+        return month_calendar
 
 class DoctorLoginPage(View):
     def get(self,request):
