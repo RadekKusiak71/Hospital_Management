8a720a4a8caeb17c060aa6659a3f55b4f993c186
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-07-02 20:26:33 +0200

    Urls added

--- main/urls.py
+++ main/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('messages/<int:msg_id>',views.MessagePage.as_view(),name='doctor_message'),
     path('perscription/',views.PerscriptionCreate.as_view(),name='doctor_perscription'),
     path('perscription/<int:user_id>/',views.PerscriptionCreateForm.as_view(),name='perscription_create'),
-    path('meetings/',views.MeetingsPage.as_view(),name='doctor_meetings'),
+    path('appointment/',views.CreateAppointment.as_view(),name='doctor_appointments'),
+    path('appointment/<int:patient_id>',views.CreateAppointmentForm.as_view(),name='doctor_appointments_create'),
+    path('patient/create/',views.CreatePatient.as_view(),name='doctor_patient_create'),
     
     path('docreg/',views.DoctorRegisterPage.as_view(),name='doctor_register_page'),
     path('doclog/',views.DoctorLoginPage.as_view(),name='doctor_login_page'),
