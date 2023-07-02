1ca57c31a56294939187740c583a4e678bda580a
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-07-02 02:23:50 +0200

    messages backed + some templates minor chanes +css

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('doclog/',views.DoctorLoginPage.as_view(),name='doctor_login_page'),
     path('doclogout/',views.DoctorLogout.as_view(),name='doctor_logout'),
     path('calendar/',views.CalendarPage.as_view(),name='doctor_calendar'),
+    path('messages/',views.MessagesPage.as_view(),name='doctor_messages'),
+    path('messages/<int:msg_id>',views.MessagePage.as_view(),name='doctor_message'),
 ]
--- main/views.py
+++ main/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth import logout,login,authenticate
 from django.utils.decorators import method_decorator
 from django.contrib.admin.views.decorators import staff_member_required
 
-from main.models import Patient,Doctor,Apointment,Medicine,Perscription,Meeting,Room
+from main.models import Patient,Doctor,Apointment,Medicine,Perscription,Meeting,Room,Message
 from .forms import MessageForm,DoctorRegisterForm,DoctorUserLoginForm
 
 method_decorator(staff_member_required,name='dispatch')
@@ -105,6 +105,14 @@ class CalendarPage(View):
                     days_list = self.update_list(appointments,days)
                     return redirect('doctor_calendar')
             
+            elif 'search_query' in request.GET:
+                CalendarPage.today = datetime.date.today()
+                calendar = self.get_month(CalendarPage.today)
+                days = self.get_days(calendar)
+                patients = self.search_query()
+                appointments = self.search_appointments(patients)
+                days_list = [[apointment] for apointment in appointments]
+            
             context = {'calendar': calendar, 'days': days_list,'today_date':day,'appointments':appointments}
             return render(request, 'main/doctor_calendar.html', context)
         else:
@@ -117,7 +125,7 @@ class CalendarPage(View):
                 if day[0].strftime("%Y-%m-%d") == appointment.date.strftime("%Y-%m-%d"):
                     day.append(appointment)
         return days_l
-
+    
     def get_calendar(self,today):
         cal = calendar.Calendar()
         month_calendar = list(cal.monthdatescalendar(today.year,today.month))
@@ -140,6 +148,67 @@ class CalendarPage(View):
         month_calendar = list(cal.monthdatescalendar(today.year,today.month))
         return month_calendar
 
+    def search_query(self):
+        patient = self.request.GET.get('search_query')
+        patient_list = []
+        
+        if Patient.objects.filter(firstname__contains = patient) is not None:
+            for patient_data in Patient.objects.filter(firstname__contains = patient):
+                patient_list.append(patient_data)
+        elif Patient.objects.filter(lastname__contains = patient) is not None:
+            print(Patient.objects.filter(lastname__contains = patient))
+            for patient_data in Patient.objects.filter(lastname__contains = patient):
+                patient_list.append(patient_data)
+        return patient_list
+
+    def search_appointments(self,patients):
+        appointment_list = []
+        for person in patients:
+            appointments = Apointment.objects.filter(patient = person).order_by('date')
+            for appointment in appointments:
+                appointment_list.append(appointment)
+        return appointment_list
+    
+class MessagesPage(View):
+    def get(self,request):
+        if request.user.is_staff:
+            msg_recived = Message.objects.filter(reciver = request.user)
+            msg_sended = Message.objects.filter(sender = request.user)
+            context = {'recived':msg_recived,'sended':msg_sended}
+            return render(request,'main/messages.html',context)
+        else:
+            return HttpResponse('Only for staff memebers')
+
+class MessagePage(View):
+    def get(self,request,msg_id):
+        if request.user.is_staff:
+            message = Message.objects.get(id=msg_id)
+            message.readed = True
+            message.save()
+            context = {'message':message}
+            return render(request,'main/message.html',context)
+        else:
+            return HttpResponse('Only for staff members')
+    def post(self,request,msg_id):
+        if 'unread' in request.POST:
+            return self.change_status(msg_id)
+        if 'delete' in request.POST:
+            return self.delete_msg(msg_id)
+
+    def delete_msg(self,msg_id):
+        message = Message.objects.get(id=msg_id).delete()
+        return redirect('doctor_messages')
+    
+    def change_status(self,msg_id):
+        message = Message.objects.get(id=msg_id)
+        if message.readed:
+            message.readed = False
+            message.save()
+        else:
+            message.readed = True
+            message.save()
+        return redirect('doctor_messages')
+    
 class DoctorLoginPage(View):
     def get(self,request):
         form = DoctorUserLoginForm()
